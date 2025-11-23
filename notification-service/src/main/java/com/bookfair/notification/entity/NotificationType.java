f60297e78204be5f62a63f55b5b24edd94be7e28
B.K.K.N Rodrigo <123990274+kavindunisansala@users.noreply.github.com>
2025-11-23 22:15:41 +0530

    Revert "Notification srvice"

--- notification-service/src/main/java/com/bookfair/notification/entity/NotificationType.java
+++
@@ -1,8 +0,0 @@
-package com.bookfair.notification.entity;
-
-public enum NotificationType {
-    RESERVATION_CONFIRMATION,
-    RESERVATION_CANCELLATION,
-    RESERVATION_UPDATE,
-    PAYMENT_CONFIRMATION
-}
