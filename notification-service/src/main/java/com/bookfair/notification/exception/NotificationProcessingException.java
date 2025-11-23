11de3b185a8b6a2a72506af1c251b4a10374cd72
Kavindu <kavindunisansalazgh@gmail.com>
2025-11-23 19:09:45 +0530

    Add global exception handler and custom exceptions for error handling

---
+++ notification-service/src/main/java/com/bookfair/notification/exception/NotificationProcessingException.java
@@ -0,0 +1,11 @@
+package com.bookfair.notification.exception;
+
+public class NotificationProcessingException extends RuntimeException {
+    public NotificationProcessingException(String message) {
+        super(message);
+    }
+
+    public NotificationProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ notification-service/src/main/java/com/bookfair/notification/exception/QRCodeGenerationException.java
@@ -0,0 +1,11 @@
+package com.bookfair.notification.exception;
+
+public class QRCodeGenerationException extends RuntimeException {
+    public QRCodeGenerationException(String message) {
+        super(message);
+    }
+
+    public QRCodeGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
