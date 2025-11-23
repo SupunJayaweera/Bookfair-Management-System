fca296ce995bfcd3df58079fb66005bd4788b185
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2025-11-23 12:37:40 +0530

    remove unwanted code

--- bookfair-backend/reservation-service/src/main/java/com/bookfair/reservation/client/StallClient.java
+++ bookfair-backend/reservation-service/src/main/java/com/bookfair/reservation/client/StallClient.java
@@ -1,7 +1,6 @@
 package com.bookfair.reservation.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
