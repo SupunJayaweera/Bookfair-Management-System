6e7602f68c4e7ee164d59e3e09161b1d973cb583
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2025-11-25 00:31:37 +0530

    added verify reservation page added the path : frontend

--- bookfair-frontend/user-portal/src/App.jsx
+++ bookfair-frontend/user-portal/src/App.jsx
@@ -4,16 +4,15 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
-
 import Login from "./pages/Login";
-import Register from "./pages/Register";   // <-- added
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import StallSelection from "./pages/StallSelection";
+import MyReservations from "./pages/MyReservations";
+import VerifyReservation from "./pages/VerifyReservation";
 import PrivateRoute from "./components/PrivateRoute";
-import MyReservations from "./pages/MyReservations"; 
-
 import "./App.css";
 
 function App() {
@@ -22,15 +21,10 @@ function App() {
       <Router>
         <div className="App">
           <Toaster position="top-right" />
-
           <Routes>
-            {/* Login Page */}
             <Route path="/login" element={<Login />} />
-
-            {/* Register Page */}
-            <Route path="/register" element={<Register />} />   {/* <-- added */}
-
-            {/* Dashboard */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/verify" element={<VerifyReservation />} />
             <Route
               path="/dashboard"
               element={
@@ -39,8 +33,22 @@ function App() {
                 </PrivateRoute>
               }
             />
-
-            {/* Default route → redirect to login */}
+            <Route
+              path="/stalls"
+              element={
+                <PrivateRoute>
+                  <StallSelection />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/my-reservations"
+              element={
+                <PrivateRoute>
+                  <MyReservations />
+                </PrivateRoute>
+              }
+            />
             <Route path="/" element={<Navigate to="/login" replace />} />
           </Routes>
         </div>
